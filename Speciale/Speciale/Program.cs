4e02832d2653e1fd10d5eb3a5d954d6b18fd9acc
viktor jeppesen <viktor.jeppesen@hotmail.com>
2020-05-25 16:06:45 +0200

    Test

--- Speciale/Speciale/Program.cs
+++ Speciale/Speciale/Program.cs
@@ -10,17 +10,21 @@ namespace Speciale
     {
         static void Main(string[] args)
         {
-            var TimeHorizon = 100;
+            //var TimeHorizon = 100;
 
-            var testESG = new BSESG(T:TimeHorizon);
-            var testscenario = testESG.Simulate();
+            //var testESG = new BSESG(T:TimeHorizon);
+            //var testscenario = testESG.Simulate();
 
 
-            //var simpletest = new FinancialObject(null, 1);
-            var test = new FinancialObject2(0.3, 1, TimeHorizon, testscenario);
+            ////var simpletest = new FinancialObject(null, 1);
+            //var test = new FinancialObject2(0.3, 1, TimeHorizon, testscenario);
 
-            //simpletest.WriteData("C:/Users/Steffen/Dropbox (Keylane)/Speciale/Data/simpletest.csv");
-            test.WriteData("C:/Users/Steffen/Dropbox (Keylane)/Speciale/Data/test.csv");
+            ////simpletest.WriteData("C:/Users/Steffen/Dropbox (Keylane)/Speciale/Data/simpletest.csv");
+            //test.WriteData("C:/Users/Steffen/Dropbox (Keylane)/Speciale/Data/test.csv");
+
+            var intensititer = new intensityObject(0.1, 0.1, 0.1, null, null, null, null, 40, 1, 0, 0);
+            intensititer.simulateMu();
+            intensititer.exportTxt();
         }
     }
 }
